91946247505aafa915878f931af5bb7b32a8e989
Maksym Pugach <pugach.m@gmail.com>
2016-04-22 09:54:19 +0300

    stop event modification

--- react-slider.js
+++ react-slider.js
@@ -15,8 +15,7 @@
   function pauseEvent(e) {
     if (e.stopPropagation) e.stopPropagation();
     if (e.preventDefault) e.preventDefault();
-    e.cancelBubble = true;
-    e.returnValue = false;
+
     return false;
   }
 
